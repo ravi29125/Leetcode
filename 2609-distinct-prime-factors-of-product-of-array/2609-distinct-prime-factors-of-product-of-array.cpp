179b8508d54282b1f7f86acee06bef723ae9fc1f
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2024-09-25 14:48:02 +0530

    Time: 259 ms (5.19%), Space: 22.8 MB (26.23%) - LeetHub

--- 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.cpp
+++ 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.cpp
@@ -1,5 +1,4 @@
 class Solution {
-    vector<bool> arr;
     unordered_map<int,int> mp;
     void help(int num){
         for(int i=2;i<=1001;i++){
@@ -12,15 +11,7 @@ class Solution {
     }
 public:
     int distinctPrimeFactors(vector<int>& nums) {
-        arr=vector<bool>(1001,true);
         mp.clear();
-        for(int i=2;i*i<=1001;i++){
-            for(int j=i*i;j<1001;j+=i){
-                arr[j]=false;
-            }
-        }
-        arr[1]=false;
-        arr[0]=false;
         for(int i:nums){
             help(i);
         }
