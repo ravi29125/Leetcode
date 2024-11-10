0a6df9993c813623f42285a9e7e01b70d9208143
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2024-11-10 10:21:43 +0530

    Time: 3 ms (20.95%), Space: 26.7 MB (31.49%) - LeetHub

--- 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.cpp
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.cpp
@@ -4,11 +4,11 @@ public:
         int n=nums.size(),mi=INT_MAX,our;
         for(int i=0;i<n;i++){
             our=nums[i];
-            if(our>=k)mi=min(mi,1);
+            if(our>=k)return 1;
             for(int j=i;j<n;j++){
                 our|=nums[j];
                 if(our>=k){
-                    mi=min(mi,j-i+1);//cout<<our<<" "<<j-i+1<<endl;
+                    mi=min(mi,j-i+1);
                 }
             }
         }
