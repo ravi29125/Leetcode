c099199db81f73d2ecfde1e62a975338c6a51e11
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2024-11-10 16:27:08 +0530

    Time: 3 ms (30.33%), Space: 8.8 MB (40.48%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long ans=x;
-        n--;
-        while(n--){
-            ans=(ans+1)|x;
+        long long ans=x,mask;
+        vector<int> bn(64);
+        long long val=n-1;
+        for(int i=0;i<64;i++){
+            bn[i]=(val&1);
+            val>>=1;
+        }
+        int j=0;
+        for(int i=0;i<64;i++){
+            mask=(1LL<<i);
+            if((mask&ans)==0){
+                if(bn[j]==1)ans=mask|ans;
+                j++;
+            }
         }return ans;
     }
 };
