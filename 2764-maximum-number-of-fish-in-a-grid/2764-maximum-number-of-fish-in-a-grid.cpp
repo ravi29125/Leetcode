35c55e32fc59a65d555e9b8f6bc55d0a4280520b
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2025-01-28 11:01:51 +0530

    Time: 47 ms (9.98%), Space: 106.3 MB (25.92%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,24 +1,14 @@
 class Solution {
     int ans;
     vector<vector<int>> arr={{0,1},{1,0},{0,-1},{-1,0}};
-    int help(int p,int qq,vector<vector<int>>& grid,vector<vector<bool>>& mp){
-        int sum=grid[p][qq];
-        queue<pair<int,int>> q;
-        q.push({p,qq});
-        while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                auto k=q.front();
-                q.pop();
-                int i=k.first,j=k.second;
-                for(auto k:arr){
-                    int x=i+k[0],y=j+k[1];
-                    if(x<grid.size() && x>=0 && y<grid[0].size() && y>=0 && grid[x][y]>0 && !mp[x][y]){
-                        sum+=grid[x][y];
-                        mp[x][y]=true;
-                        q.push({x,y});
-                    }
-                }
+    int help(int i,int j,vector<vector<int>>& grid,vector<vector<bool>>& mp,int sum){
+        if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0)return 0;
+        sum+=grid[i][j];
+        for(auto k:arr){
+            int x=i+k[0],y=j+k[1];
+            if(x<grid.size() && x>=0 && y<grid[0].size() && y>=0 && grid[x][y]>0 && !mp[x][y]){
+                mp[x][y]=true;
+                sum+=help(x,y,grid,mp,0);
             }
         }
         return sum;
@@ -31,11 +21,8 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]>0 && !mp[i][j]){
-                    //cout<<i<<" "<<j<<" ";
                     mp[i][j]=true;
-                    int mx=help(i,j,grid,mp);//mp[i][j]=false;
-                    cout<<mx<<endl;
-                    ans=max(mx,ans);
+                    ans=max(ans,help(i,j,grid,mp,0));
                 }
             }
         }return ans;
