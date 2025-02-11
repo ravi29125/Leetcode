b8124966a2207f387c3043b744c9fa2d1d7cfb64
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2025-02-11 22:05:59 +0530

    Time: 282 ms (39.43%), Space: 78 MB (7.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
-    int ans;
-    void help(vector<int>& nums,int target,long long sum,int i){
+    map<pair<int,long long>,int>mp;
+    int help(vector<int>& nums,int target,long long sum,int i){
         if(i==nums.size()){
-            if(sum==target)ans++;
-            return ;
+            if(sum==target) return 1;
+            else return 0;
         }
-        help(nums,target,sum+nums[i],i+1);
-        help(nums,target,sum-nums[i],i+1);
+        if(mp.find({i,sum})!=mp.end()) return mp[{i,sum}];
+        return mp[{i,sum}]=help(nums,target,sum+nums[i],i+1)+help(nums,target,sum-nums[i],i+1);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        ans=0;
-        help(nums,target,0,0);
-        return ans;
+        return help(nums,target,0,0);
     }
 };
