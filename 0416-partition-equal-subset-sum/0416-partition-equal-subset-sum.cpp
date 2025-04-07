b9518e9bf2585beeed447e30be8e80a0111fe281
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2025-04-07 08:59:40 +0530

    Time: 72 ms (82.28%), Space: 13.4 MB (86.66%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,9 +1,4 @@
 class Solution {
-    bool help(int n,int i,vector<int>& nums,int s1,int s2){
-        if(i==n){
-            return s1==s2;
-        }return help(n,i+1,nums,s1+nums[i],s2) || help(n,i+1,nums,s1,nums[i]+s2); 
-    }
 public:
     bool canPartition(vector<int>& nums) {
         int sum=0;
