19dfc8b3d39a0cb9b697ad564955ee53ce0fd4a3
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2025-04-20 11:55:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (10.59%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& nums) {
+        int n=nums.size();
+        unordered_set<int> s;
+        unordered_map<int,int> mp;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            s.insert(nums[i]);
+            if(nums[i]==0)cnt++;
+            mp[nums[i]]++;
+        }
+        int ans=0;
+        for(int i:s)if(i!=0){
+            //if(mp[i]>1 && i<mp[i])
+            ans+=((mp[i]+i)/(i+1))*(i+1);
+        }
+        //cout<<cnt<<" ";
+        return ans+cnt;
+    }
+};
